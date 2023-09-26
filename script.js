3545ca54d35446096484eb3556b9ce84a1833bf4
Oliver D'Adda <olidadda@gmail.com>
2023-09-26 20:32:36 +0200

    Adding dropdown Stars

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
+// Sample star data (you can expand this with more stars and their coordinates)
+const starData = {
+  Betelgeuse: { ra: "88.8", dec: "7.4" },
+  Bellatrix: { ra: "81.28", dec: "6.35" },
+  Epsilon: { ra: "281.08", dec: "39.67" },
+  Vega: { ra: "279.23", dec: "38.78" },
+  Zeta: { ra: "281.19", dec: "37.60" },
+  Delta: { ra: "283.62", dec: "36.90" },
+  Sulafat: { ra: "284.73", dec: "32.69" },
+  Sheliak: { ra: "282.52", dec: "33.36" },
+  KappaSaif: { ra: "86.94", dec: "-9.67" },
+  Rigel: { ra: "78.63", dec: "-8.20" },
+  Mintaka: { ra: "83.00", dec: "-0.30" },
+  EpsilonAlnilam: { ra: "84.05", dec: "-1.20" },
+  Alnitak: { ra: "85.19", dec: "-1.94" },
+  Heka: { ra: "83.88", dec: "10.00" },
+  AlSaif: { ra: "83.86", dec: "-5.91" },
+  // ... add more stars as needed
+};
+
+document.getElementById("star1Select").addEventListener("change", function () {
+  const selectedStar = this.value;
+  document.getElementById("star1RA").value = starData[selectedStar].ra;
+  document.getElementById("star1Dec").value = starData[selectedStar].dec;
+});
+
+// Repeat for Star 2
+document.getElementById("star2Select").addEventListener("change", function () {
+  const selectedStar = this.value;
+  document.getElementById("star2RA").value = starData[selectedStar].ra;
+  document.getElementById("star2Dec").value = starData[selectedStar].dec;
+});
+
 document
   .getElementById("starForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Get the values from the input fields
     const star1RA = parseFloat(document.getElementById("star1RA").value);
     const star1Dec = parseFloat(document.getElementById("star1Dec").value);
     const star2RA = parseFloat(document.getElementById("star2RA").value);
